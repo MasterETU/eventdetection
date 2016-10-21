ffe550e1cee76c15f0daafe616ac7d01663a3a8a
Tolga YILMAZ <tolgayilmaz86@gmail.com>
2016-10-21 16:54:53 +0300

    init

---
+++ src/main/java/EventDetectionMain.java
@@ -0,0 +1,15 @@
+/**
+ * Created by tolga on 21.10.2016.
+ */
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
+
+
+import java.io.File;
+
+public class EventDetectionMain {
+    public static void main(String [] args){
+
+    }
+}
